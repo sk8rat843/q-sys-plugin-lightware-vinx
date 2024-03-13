ef0e9452ef4a5159372b9c921b549b54ce656ca1
Isaac Porter <Isaac.Porter@cti.com>
2024-03-13 09:39:23 -0600

    chore: brokeout plugin to use plugin compiler

---
+++ src/Lightware-VINX/info.lua
@@ -0,0 +1,8 @@
+PluginInfo = {
+  Name = "Lightware~VINX (v1.0)",
+  Version = "1.0.0.0",
+  Id = "5d2402d7-9d4f-40ab-a8f6-0cddf404b1bb",
+  Description = "Control for Lightware VINX",
+  ShowDebug = false,
+  Author = "Isaac Porter"
+}
